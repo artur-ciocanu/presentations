9f140209571591ebd6ff3040e08b5259c62315b9
Artur Ciocanu <ciocanu@adobe.com>
2024-09-02 15:26:55 +0300

    Adding demo-app code

---
+++ apidays-conf-london-2024/code/demo-app/src/main/java/org/demo/app/MetricsDataController.java
@@ -0,0 +1,22 @@
+package org.demo.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+public class MetricsDataController {
+
+    @GetMapping("/metrics")
+    public MetricsData getMetricsData() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        return new MetricsData(
+                random.nextInt(20, 85),
+                random.nextInt(2048, 4096),
+                random.nextInt(300, 1000)
+        );
+    }
+}
